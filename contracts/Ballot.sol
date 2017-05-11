5f565058b15c52f1069782e6abff2a23b8a7ec6f
meghanay <meghana.yadagiri@rhythmone.com>
2017-05-11 17:35:49 +0530

    updating Ballot.sol,testVoting.js files further

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -2,61 +2,61 @@ pragma solidity ^0.4.0;
 
 
 contract Ballot {
-	mapping (uint => Candidate) public Ballot;
+	mapping (uint => Candidate) public ballot;
 	struct Candidate {
-		address candidate_account;
-		uint vote_count;
+		address candidateAccount;
+		uint voteCount;
 	}
-	uint public Numcandidates;
+	uint public numCandidates;
 
 
 	function Ballot(address[] addresses) {
 		for (uint i=0; i < addresses.length; i++) {
-			Ballot[Numcandidates++] = Candidate(addresses[i],0);
+			ballot[numCandidates++] = Candidate(addresses[i], 0);
 		}
 	}
 
-	function Totalvotesreceived(address account) constant returns (uint) {
-		if (Validcandidate(account) == false) throw;
-		for (uint i=0; i < Numcandidates; i++) {
-			if (Ballot[i].candidate_account == account) {
-				return Ballot[i].vote_count;
+	function totalVotesReceived(address account) constant returns (uint) {
+		if (validCandidate(account) == false) throw;
+		for (uint i=0; i < numCandidates; i++) {
+			if (ballot[i].candidateAccount == account) {
+				return ballot[i].voteCount;
 			}
 		}
 	}
 
-	function Votefor(address account) {
-		if (Validcandidate(account) == false) throw;
-		for (uint i=0; i < Numcandidates; i++) {
-			if (Ballot[i].candidate_account == account) {
-				Ballot[i].vote_count++;
+	function voteFor(address account) {
+		if (validCandidate(account) == false) throw;
+		for (uint i=0; i < numCandidates; i++) {
+			if (ballot[i].candidateAccount == account) {
+				ballot[i].voteCount++;
 			}
 		}
 	}
 
-	function Totalcandidates() constant returns (uint) {
-		return Numcandidates;
+	function totalCandidates() constant returns (uint) {
+		return numCandidates;
 	}
 
-	function Validcandidate(address account) constant returns (bool) {
-		for (uint i=0; i < Numcandidates; i++) {
-			if (Ballot[i].candidate_account == account) {
+	function validCandidate(address account) constant returns (bool) {
+		for (uint i=0; i < numCandidates; i++) {
+			if (ballot[i].candidateAccount == account) {
 				return true;
 			} 
 		}
-		return false;
+		throw;
 	}
 
-	function Findwinner() returns (address) {
-		uint winning_count;
+	function findWinner() returns (address) {
+		uint winningCount;
 		address winner;
-		for (uint i=0; i < (Numcandidates-1); i++) {
-			if (Ballot[i].vote_count > Ballot[i+1].vote_count) {
-				winning_count = Ballot[i].vote_count;
-				winner = Ballot[i].candidate_account;
+		for (uint i=0; i < (numCandidates-1); i++) {
+			if (ballot[i].voteCount > ballot[i+1].voteCount) {
+				winningCount = ballot[i].voteCount;
+				winner = ballot[i].candidateAccount;
 			}else {
-				winning_count = Ballot[i+1].vote_count;
-				winner = Ballot[i+1].candidate_account;
+				winningCount = ballot[i+1].voteCount;
+				winner = ballot[i+1].candidateAccount;
 			}
 		}
 		return winner;
--- test/testVoting.js
+++ test/testVoting.js
@@ -1,4 +1,6 @@
 'use strict';
+import expectThrow from './helpers/expectThrow';
+
 const Ballot = artifacts.require('../contracts/Ballot.sol');
 
 contract('Ballot', function(accounts) {
@@ -8,39 +10,42 @@ contract('Ballot', function(accounts) {
 		ballot = await Ballot.new([0x0, accounts[0]]);
 	});
 
-	describe('TotalCandidates', function() {
+	describe('totalCandidates', function() {
 		it('should check the count of candidates', async function() {
-			let candidate_count = await ballot.Totalcandidates();
+			let candidate_count = await ballot.totalCandidates();
 			assert.equal(candidate_count.valueOf(), 2, 'Above candidates got added.');
 		});
 	});
 
-	describe('ValidCandidate', function() {
+	describe('validCandidate', function() {
         it('should check if a candidate is valid', async function() {
-        	let validity_status = await ballot.Validcandidate.call(0x0);
+        	let validity_status = await ballot.validCandidate.call(0x0);
         	assert.equal(validity_status, true, 'Above candiate is valid.');
+        });
+        it('should throw if a candidate is not valid.', async function() {
+        	await expectThrow(ballot.validCandidate.call(accounts[1]));
         });
     });
 
 
-	describe('TotalVotesReceived', function() {
+	describe('totalVotesReceived', function() {
         it('should provide votes received', async function() {
-        	await ballot.Votefor(accounts[0]);
-        	await ballot.Votefor(accounts[0]);
-        	await ballot.Votefor(0x0);
-        	let votecount = await ballot.Totalvotesreceived.call(0x0);
+        	await ballot.voteFor(accounts[0]);
+        	await ballot.voteFor(accounts[0]);
+        	await ballot.voteFor(0x0);
+        	let votecount = await ballot.totalVotesReceived.call(0x0);
         	assert.equal(votecount.valueOf(), 1, 'Above candidate has received 1 votes.');
         });
     });
 
-    describe('FindWinner', function() {
+    describe('findWinner', function() {
     	it('should return the winning address', async function() {
-    		await ballot.Votefor(accounts[0]);
-        	await ballot.Votefor(accounts[0]);
-        	await ballot.Votefor(accounts[0]);
-        	await ballot.Votefor(0x0);
-        	await ballot.Votefor(0x0);
-    		let winner = await ballot.Findwinner.call();
+    		await ballot.voteFor(accounts[0]);
+        	await ballot.voteFor(accounts[0]);
+        	await ballot.voteFor(accounts[0]);
+        	await ballot.voteFor(0x0);
+        	await ballot.voteFor(0x0);
+    		let winner = await ballot.findWinner.call();
     		assert.equal(winner.valueOf(), accounts[0], 'Found a winner.');
     	});
     });
